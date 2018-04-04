6c6aff595dcfa961cfdafe7fe45b1e24646544cc
ericadavis <ericadavis@172.16.21.160>
2018-04-04 13:10:42 -0400

    Personalized!!

--- src/Cell.java
+++ src/Cell.java
@@ -3,106 +3,153 @@
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-
 public class Cell extends JPanel {
-    private static int SIZE = 20;
-    public static final int TOP = 0;
-    public static final int RIGHT = 1;
-    public static final int BOTTOM = 2;
-    public static final int LEFT = 3;
-    private int row = -1;
-    private int col = -1;
-    private boolean [] wall = {true, true, true, true};
+	private static int SIZE = 20;
+	public static final int TOP = 0;
+	public static final int RIGHT = 1;
+	public static final int BOTTOM = 2;
+	public static final int LEFT = 3;
+	private int row = -1;
+	private int col = -1;
+	private boolean [] wall = {true, true, true, true};
     
-    private boolean current = false;
-    private boolean end = false;
+	private boolean current = false;
+	private boolean end = false;
     
-    private boolean [] path = {false, false, false, false};
-    
-    
-    public Cell(int r, int c){
-        row = r;
-        col = c;
-    }
-    
-    public int getRow(){
-        return row;
-    }
-    
-    public int getCol(){
-        return col;
-    }
-    
-    public boolean isWall(int index){
-        return wall[index];
-    }
-    
-    public boolean hasAllWalls(){
-        boolean allWalls = isWall(TOP) && isWall(BOTTOM) && 
-                isWall(LEFT) && isWall(RIGHT);
-        return allWalls;
-    }
-    
-    public void removeWall(int w){
-        wall[w] = false;
-        repaint();
-    }
+	private boolean [] path = {false, false, false, false};
     
     
-    public void openTo(Cell neighbor){
-        if (row < neighbor.getRow()){
-            removeWall(BOTTOM);
-            neighbor.removeWall(TOP);
-        } else if (row > neighbor.getRow()){
-            removeWall(TOP);
-            neighbor.removeWall(BOTTOM);
-        } else if (col < neighbor.getCol()){
-            removeWall(RIGHT);
-            neighbor.removeWall(LEFT);
-        } else if (col > neighbor.getCol()){
-            removeWall(LEFT);
-            neighbor.removeWall(RIGHT);
-        }
-    }
+	public Cell(int r, int c){
+		row = r;
+		col = c;
+	}
     
-    public void setCurrent(boolean curr){
-        current = curr;
-        repaint();
-    }
+	public int getRow(){
+		return row;
+	}
     
-    public void setEnd(boolean e){
-        end = e;
-        repaint();
-    }
+	public int getCol(){
+		return col;
+	}
     
-    public void addPath(int side){
-        path[side] = true;
-        repaint();
-    }
+	public boolean isWall(int index){ // is there a wall there?
+		return wall[index];
+	}
     
-    @Override
-    public Dimension getPreferredSize(){
-        return new Dimension (SIZE, SIZE);
-    }
+	public boolean hasAllWalls(){ // use when randomizing maze so that there are no traps
+		boolean allWalls = isWall(TOP) && isWall(BOTTOM) && 
+				isWall(LEFT) && isWall(RIGHT);
+		return allWalls;
+	}
     
-    @Override
-    public void paintComponent(Graphics g){
-        //draw the background
-        g.setColor(Color.WHITE);
-        g.fillRect(0, 0, SIZE, SIZE);
-        g.setColor(Color.BLACK);
+	public void removeWall(int w){ //if there's a trap, remove a wall
+		wall[w] = false;
+		repaint();
+	}
+    
+    
+	public void openTo(Cell neighbor){ //make sure there's an end
+		if (row < neighbor.getRow()){
+			removeWall(BOTTOM);
+			neighbor.removeWall(TOP);
+		} else if (row > neighbor.getRow()){
+			removeWall(TOP);
+			neighbor.removeWall(BOTTOM);
+		} else if (col < neighbor.getCol()){
+			removeWall(RIGHT);
+			neighbor.removeWall(LEFT);
+		} else if (col > neighbor.getCol()){
+			removeWall(LEFT);
+			neighbor.removeWall(RIGHT);
+		}
+	}
+	
+	 
+	public void setCurrent(boolean curr){
+		current = curr;
+		repaint();
+	}
+    
+	public void setEnd(boolean e){
+		end = e;
+		repaint();
+	}
+    
+	public void addPath(int side){
+		path[side] = true;
+		repaint();
+	}
+    
+	@Override
+	public Dimension getPreferredSize(){
+		return new Dimension (SIZE, SIZE);
+	}
+    
+	@Override
+	 
+	public void paintComponent(Graphics g){
+		//draw the background
+		g.setColor(Color.WHITE);
+		g.fillRect(0, 0, SIZE, SIZE);
+		g.setColor(Color.BLACK);
         
-        //draw the walls
-        
-        
-        //draw the path
-        g.setColor(Color.GREEN);
-        
-        
-        //draw the balls
-        
+		//draw the walls
+//		if (isWall(RIGHT)) {
+//			System.out.println("draw right wall");
+//			g.drawLine(SIZE, 0, SIZE, SIZE); //side line
+//		}
+		if (isWall(LEFT)) {
+			//System.out.println("draw left wall");
+			g.drawLine(0, 0, 0, SIZE); //side line
+		}
+		if (isWall(TOP)) {
+			g.drawLine(0, 0, SIZE, 0); //top line
+		}
+		
+		
+
+		//draw the path
+		g.setColor(Color.PINK);
+		
+		if (path[BOTTOM]) {
+			g.setColor(Color.RED);
+			g.drawLine(SIZE/2, SIZE/2, SIZE/2, SIZE);
+		}
+		if (path[TOP]) {
+			g.setColor(Color.ORANGE);
+			g.drawLine(SIZE/2, 0, SIZE/2, SIZE/2);
+		}
+		if (path[RIGHT]) {
+			g.setColor(Color.YELLOW);
+			g.drawLine(SIZE/2, SIZE/2, SIZE, SIZE/2);
+		}
+		if (path[LEFT]) {
+			g.setColor(Color.GREEN);
+			g.drawLine(0, SIZE/2, SIZE/2, SIZE/2);
+		}
+		ImageIcon unicorn = new ImageIcon("unicorn-2007266_960_720.png");
+		Image image2 = unicorn.getImage();
+		//draw the balls
+		if (current == true) {  
+			//g.setColor(Color.GREEN);
+			//g.fillOval(0 + SIZE/4, 0 + SIZE/4, SIZE - SIZE/2, SIZE - SIZE/2);
+			g.drawImage(image2, 0, 0, SIZE, SIZE, null);
+		}
+		if (end == true) {
+			g.setColor(Color.RED);
+			g.fillOval(0 + SIZE/4, 0 + SIZE/4, SIZE - SIZE/2, SIZE - SIZE/2);
+		}
     }
     
     
--- src/MazeGenerator.java
+++ src/MazeGenerator.java
@@ -20,177 +20,213 @@ import javax.swing.UIManager;
 
 public class MazeGenerator extends JFrame {
     
-    private int rows = 5;
-    private int cols = 5;
-    private Cell [][] grid = new Cell[rows][cols];
-    private JPanel mazePanel = new JPanel();
-    private int row = 0;
-    private int col = 0;
-    private int endRow = rows - 1;
-    private int endCol = cols - 1;
+	private int rows = 10;
+	private int cols = 10;
+	private Cell [][] grid = new Cell[rows][cols];
+	private JPanel mazePanel = new JPanel();
+	private int row = 0;
+	private int col = 0;
+	private int endRow = rows - 1; //because we start at 0
+	private int endCol = cols - 1;
 
-    public MazeGenerator(){
-        initGUI();
+	public MazeGenerator(){
+		initGUI();
         
-        setTitle("Maze");
-        setResizable(false);
-        pack();
-        setLocationRelativeTo(null);
-        setVisible(true);
-        //setSize(200, 200);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        
-        
-    }
+		setTitle("Unicorn Maze");
+		setResizable(false);
+		pack();
+		setLocationRelativeTo(null);
+		setVisible(true);
+		//setSize(200, 200);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
     
-    private void initGUI(){
-        //title 
-        JPanel titlePanel = new JPanel(); 
-        titlePanel.setBackground(Color.BLACK);
-        add(titlePanel, BorderLayout.PAGE_START);
-        Font titleFont = new Font("Fish&Chips", Font.BOLD, 32);
-        JLabel titleLabel = new JLabel("Maze");
-        titleLabel.setBackground(Color.BLACK);
-        titleLabel.setOpaque(true);
-        titleLabel.setForeground(Color.WHITE);
-        titleLabel.setFont(titleFont);
-        titleLabel.setHorizontalAlignment(JLabel.CENTER);
-        titlePanel.add(titleLabel);
+	private void initGUI(){
+		//title 
+		JPanel titlePanel = new JPanel(); 
+		titlePanel.setBackground(Color.BLACK);
+		add(titlePanel, BorderLayout.PAGE_START);
+		Font titleFont = new Font("Fish&Chips", Font.BOLD, 32);
+		JLabel titleLabel = new JLabel("Maze");
+		titleLabel.setBackground(Color.BLACK);
+		titleLabel.setOpaque(true);
+		titleLabel.setForeground(Color.WHITE);
+		titleLabel.setFont(titleFont);
+		titleLabel.setHorizontalAlignment(JLabel.CENTER);
+		titlePanel.add(titleLabel);
         
-        //center panel
-        JPanel centerPanel = new JPanel();
-        centerPanel.setBackground(Color.BLACK);
-        add(centerPanel, BorderLayout.CENTER);
+		//center panel
+		JPanel centerPanel = new JPanel();
+		centerPanel.setBackground(Color.BLACK);
+		add(centerPanel, BorderLayout.CENTER);
         
-        //maze panel
-        newMaze();
-        centerPanel.add(mazePanel);
+		//maze panel
+		newMaze();
+		centerPanel.add(mazePanel);
         
-        //button panel
-        JPanel buttonPanel = new JPanel();
-        buttonPanel.setBackground(Color.BLACK);
-        add(buttonPanel, BorderLayout.PAGE_END);
-        JButton newMazeButton = new JButton("New Maze");
-        newMazeButton.setFocusable(false);
-        buttonPanel.add(newMazeButton, BorderLayout.CENTER);
+		//button panel
+		JPanel buttonPanel = new JPanel();
+		buttonPanel.setBackground(Color.BLACK);
+		add(buttonPanel, BorderLayout.PAGE_END);
+		JButton newMazeButton = new JButton("New Maze");
+		newMazeButton.setFocusable(false);
+		buttonPanel.add(newMazeButton, BorderLayout.CENTER);
         
+
+		//listeners
+		newMazeButton.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				newMaze();
+			} 
+		});
         
-        
-        //listeners
-        newMazeButton.addActionListener(new ActionListener(){
-           public void actionPerformed(ActionEvent e){
-               newMaze();
-           } 
-        });
-        
-        addKeyListener(new KeyAdapter() {
-            public void keyReleased(KeyEvent e){
-                //char c = e.getKeyChar();
-                //System.out.println(c);
-                int keyCode = e.getKeyCode();
-                //moveBall(keyCode);
-            }       
-        });        
-    }
+	addKeyListener(new KeyAdapter() {
+		public void keyReleased(KeyEvent e){
+			//char c = e.getKeyChar();
+			//System.out.println(c);
+			int keyCode = e.getKeyCode();
+			moveBall(keyCode);
+		}       
+		});
+	}
     
-    private void moveBall(int direction){
-        switch (direction){
-            case KeyEvent.VK_UP:
-                
-                
-                break;
-            case KeyEvent.VK_DOWN:
-                
-                
-                break;
-            case KeyEvent.VK_LEFT:
-                
-                
-                break;
-            case KeyEvent.VK_RIGHT:
-                
-                
-                break;
-        }
-        
-        //puzzle solved?
-        if (row == endRow && col == endCol){
-            String message = "You won!";
-            JOptionPane.showMessageDialog(null, message);
-        }
-                
-        
-        
-    }
+	private void moveBall(int direction){
+		switch (direction){
+			case KeyEvent.VK_UP:
+				if (!grid[row][col].isWall(Cell.TOP)) {
+					moveTo(row - 1, col, Cell.TOP, Cell.BOTTOM);
+				}
+				break;
+				
+			case KeyEvent.VK_DOWN:
+				//move one cell down if no wall
+				if (!grid[row][col].isWall(Cell.BOTTOM)) {
+					moveTo(row + 1, col, Cell.BOTTOM, Cell.TOP);
+				}
+				break;
+				
+			case KeyEvent.VK_LEFT:
+				if (!grid[row][col].isWall(Cell.LEFT)) {
+					moveTo(row, col - 1, Cell.LEFT, Cell.RIGHT);
+				}	
+				break;
+				
+			case KeyEvent.VK_RIGHT:
+				if (!grid[row][col].isWall(Cell.RIGHT)) {
+					moveTo(row, col + 1, Cell.RIGHT, Cell.LEFT);
+				}
+				break;
+		}
+
+		//puzzle solved?
+		if (row == endRow && col == endCol){
+			String message = "You won!";
+			JOptionPane.showMessageDialog(null, message);
+			newMaze();
+		}
+		
+	}
     
-    private void moveTo(int nextRow, int nextCol, 
-            int firstDirection, int secondDirection){
+	private void moveTo(int nextRow, int nextCol, 
+			int firstDirection, int secondDirection){
+		grid[row][col].setCurrent(false);
+		grid[row][col].addPath(firstDirection); //now we're in a new cell
+		
+		row = nextRow;
+		col = nextCol;
+		
+		grid[row][col].setCurrent(true);
+		grid[row][col].addPath(secondDirection);
+
+	}
+
+	private void newMaze (){
+		mazePanel.removeAll();
+		mazePanel.setLayout(new GridLayout(rows, cols));
+		grid = new Cell [rows][cols];
         
-    }
+		for (int r = 0; r < rows; r++){
+			for (int c = 0; c < cols; c++){
+				grid[r][c] = new Cell (r, c);
+				mazePanel.add(grid[r][c]);
+			}
+		}
+
+		generateMaze();
+		row = 0;
+		col = 0;
+		endRow = rows - 1;
+		endCol = cols - 1;
+		grid[row][col].setCurrent(true);
+		grid[endRow][endCol].setEnd(true);
+		mazePanel.revalidate();
+	}
     
-    private void newMaze (){
-        mazePanel.removeAll();
-        mazePanel.setLayout(new GridLayout(rows, cols));
-        grid = new Cell [rows][cols];
+	private void generateMaze(){
+		ArrayList<Cell> tryLaterCells = new ArrayList();
+		int totalCells = rows * cols;
+		int visitedCells = 1;
         
-        for (int r = 0; r < rows; r++){
-            for (int c = 0; c < cols; c++){
-                grid[r][c] = new Cell (r, c);
-                mazePanel.add(grid[r][c]);
-            }
-        }
+		//start at a random cell
+		Random rand = new Random();
+		int r = rand.nextInt(rows);
+		int c = rand.nextInt(cols);
         
-        //generateMaze();
-        row = 0;
-        col = 0;
-        endRow = rows - 1;
-        endCol = cols - 1;
-        grid[row][col].setCurrent(true);
-        grid[endRow][endCol].setEnd(true);
-        mazePanel.revalidate();
-    }
+		//while not all cells have yet been visited
+		while (visitedCells < totalCells){
+			//find all neighbors with all walls intact
+			ArrayList<Cell> neighbors = new ArrayList();
+			
+			if (isAvailable(r-1, c)) {//top neighbor
+				neighbors.add(grid[r-1][c]); //adds to the list of neighbors
+			}
+			if (isAvailable(r+1, c)) {//bottom neighbor
+				neighbors.add(grid[r+1][c]); 
+			}
+			if (isAvailable(r, c-1)) {//left neighbor
+				neighbors.add(grid[r][c-1]); 
+			}
+			if (isAvailable(r, c+1)) {//right neighbor
+				neighbors.add(grid[r][c+1]); 
+			}
+
+			//if one or more found
+			if (neighbors.size() > 0) {
+				//if more than 1 found add this cell to the list and try again
+				if (neighbors.size() > 1) {
+					tryLaterCells.add(grid[r][c]);
+				}
+                
+				//pick a neighbor
+				int pick = rand.nextInt(neighbors.size());
+				Cell neighbor = neighbors.get(pick);
+				//remove a wall
+				grid[r][c].openTo(neighbor);
+				
+				//go to the neighbor and increment the number visited
+				r = neighbor.getRow();
+				c = neighbor.getCol();
+				visitedCells++;
+				
+			} else {
+				//if none was found, go to one of the cells that was saved to try later
+				Cell nextCell = tryLaterCells.remove(0);
+				r = nextCell.getRow();
+				c = nextCell.getCol();
+
+			}
+		}
+	}
     
-    private void generateMaze(){
-        ArrayList<Cell> tryLaterCells = new ArrayList();
-        int totalCells = rows * cols;
-        int visitedCells = 1;
-        
-        //start at a random cell
-        Random rand = new Random();
-        int r = rand.nextInt(rows);
-        int c = rand.nextInt(cols);
-        
-        //while not all cells have yet been visited
-        while (visitedCells < totalCells){
-            //find all neighbors with all walls intact
-            ArrayList<Cell> neighbors = new ArrayList();
-          
-        
-            //if one or more found
-            
-                //if more than 1 found add this cell to the list and try again
-                
-                //pick a neighbor and remove the wall
-                
-        
-                //go to the neighbor and incrememnt the number visited
-                
-                //if none was found, go to one of the cells that was saved 
-                //to try later
-               
-        
-        }
-    }
-    
-    private boolean isAvailable(int r, int c){
-        boolean available = false;
-        if (r < rows && r >= 0 && c < cols && c >= 0){
-            if (grid[r][c].hasAllWalls()){
-                available = true;
-            }
-        }
-        return available;
-    }
+	private boolean isAvailable(int r, int c){
+		boolean available = false;
+		if (r < rows && r >= 0 && c < cols && c >= 0){//checks if it's a valid cell
+			if (grid[r][c].hasAllWalls()){
+				available = true;
+			}
+		}
+		return available;
+	}
     
     public static void main(String[] args) {
         try {
